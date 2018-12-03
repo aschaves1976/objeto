79e236fe1ab6992cd414314a4333f86749bc254f
aschaves1976 <roots17@gmail.com>
2018-12-03 20:41:14 -0200

    Introdução a OO

---
+++ oo.js
@@ -0,0 +1,24 @@
+// Còdigo não executável
+
+// Procedural
+processamento (valor1, valor2, valor3)
+
+//OO
+objeto={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+objeto.processamento() // foco passou a ser o objeto, antes era a função
+
+//Principios importantes
+// 1. Abstração
+//   - Ex. Um objeto carro possui vários atributos, mas dependendo do objetivo do sistema somente alguns serão utilizados.
+// 2. Encapsulamento
+//   - Manter os detalhes (regras, cálculos) fora do alcance do usuário final, os detalhes da origem do programa não precisa ser exposto.
+// 3. Herança (prototype)
+// 4. Polimorfismo
+//
